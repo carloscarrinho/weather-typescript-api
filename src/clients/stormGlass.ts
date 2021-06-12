df4a9edf8528fedfdccc54847142ccefa59aa93e
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-12 07:34:06 -0300

    refactor: apply normalization for Storm Glass forecast data

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,78 @@
+import { AxiosStatic } from 'axios';
+
+export interface StormGlassPointSource {
+  [key: string]: number;
+}
+
+export interface StormGlassPoint {
+  readonly time: string;
+  readonly waveHeight: StormGlassPointSource;
+  readonly waveDirection: StormGlassPointSource;
+  readonly swellDirection: StormGlassPointSource;
+  readonly swellHeight: StormGlassPointSource;
+  readonly swellPeriod: StormGlassPointSource;
+  readonly windDirection: StormGlassPointSource;
+  readonly windSpeed: StormGlassPointSource;
+}
+
+export interface StormGlassForecastResponse {
+  hours: StormGlassPoint[];
+}
+
+export interface ForecastPoint {
+  time: string;
+  waveHeight: number;
+  waveDirection: number;
+  swellDirection: number;
+  swellHeight: number;
+  swellPeriod: number;
+  windDirection: number;
+  windSpeed: number;
+}
+
 export class StormGlass {
-  public async fetchPoints(lat: number, lnt: number): Promise<{}> {
-    return Promise.resolve({});
+  readonly stormGlassAPIParams =
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+
+  readonly stormGlassAPISource = 'noaa';
+
+  constructor(protected request: AxiosStatic) {}
+
+  public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
+    const response = await this.request.get<StormGlassForecastResponse>(
+      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&souce${this.stormGlassAPISource}=&end=159211380&lat=${lat}&lng=${lng}`
+    );
+
+    return this.normalizeResponse(response.data);
+  }
+
+  private normalizeResponse(
+    points: StormGlassForecastResponse
+  ): ForecastPoint[] {
+    // attention: we must use 'bind(this)' when using 'this' inside another scope (function)
+    // otherwise we'll receive an undefined because the 'this' has changed
+    return points.hours.filter(this.isValidPoint.bind(this)).map((point) => ({
+      time: point.time,
+      waveHeight: point.waveHeight[this.stormGlassAPISource],
+      waveDirection: point.waveDirection[this.stormGlassAPISource],
+      swellDirection: point.swellDirection[this.stormGlassAPISource],
+      swellHeight: point.swellHeight[this.stormGlassAPISource],
+      swellPeriod: point.swellPeriod[this.stormGlassAPISource],
+      windDirection: point.windDirection[this.stormGlassAPISource],
+      windSpeed: point.windSpeed[this.stormGlassAPISource],
+    }));
+  }
+
+  private isValidPoint(point: Partial<StormGlassPoint>): boolean {
+    return !!(
+      point.time &&
+      point.waveHeight?.[this.stormGlassAPISource] &&
+      point.waveDirection?.[this.stormGlassAPISource] &&
+      point.swellDirection?.[this.stormGlassAPISource] &&
+      point.swellHeight?.[this.stormGlassAPISource] &&
+      point.swellPeriod?.[this.stormGlassAPISource] &&
+      point.windDirection?.[this.stormGlassAPISource] &&
+      point.windSpeed?.[this.stormGlassAPISource]
+    );
   }
 }
