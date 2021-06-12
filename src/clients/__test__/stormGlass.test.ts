899482777627e23a9067623773d50849efdd613c
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-12 07:35:26 -0300

    test: stormGlass normalized forecast data

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,11 +1,17 @@
 import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormGlass3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_3_hours.json';
+
+jest.mock('axios');
 
 describe('StormGlass Client', () => {
-  it('should return the normalized forecast from the StormGlass service', async() => {
+  it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726;
     const lng = 151.299824;
-    const stormGlass = new StormGlass();
+    axios.get = jest.fn().mockResolvedValue({ data: stormGlass3HoursFixture });
+    const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
-    expect(response).toEqual({});
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
   });
 });
