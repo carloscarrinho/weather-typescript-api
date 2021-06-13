e76fdf7084687a5baa981927849dc26dfcaa2298
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-13 19:04:48 -0300

    test: use jest mock correctly

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,11 +6,12 @@ import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normali
 jest.mock('axios');
 
 describe('StormGlass Client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726;
     const lng = 151.299824;
-    axios.get = jest.fn().mockResolvedValue({ data: stormGlass3HoursFixture });
-    const stormGlass = new StormGlass(axios);
+    mockedAxios.get.mockResolvedValue({ data: stormGlass3HoursFixture });
+    const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormGlassNormalized3HoursFixture);
   });
