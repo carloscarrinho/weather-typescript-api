83714e67dd7a824506f60d52a079d10363d48a22
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-17 08:06:45 -0300

    test: create a beach with success

---
+++ test/functional/beaches.test.ts
@@ -0,0 +1,18 @@
+describe('Beaches functional tests', () => {
+  describe('When creating a beach', () => {
+    it('should create a beach with success', async () => {
+      // given
+      const newBeach = {
+        lat: -33.792726,
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+      // when
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+      // then
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual(expect.objectContaining(newBeach));
+    });
+  });
+});
