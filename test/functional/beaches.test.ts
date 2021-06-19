a7effd3a451929ac9f58a2ecaf21df8f1d8578fc
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-19 06:30:21 -0300

    test: return 422 status code when there is a validation error on beach creation

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -17,5 +17,23 @@ describe('Beaches functional tests', () => {
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach));
     });
+
+    it('should return 422 when there is a validation error', async () => {
+      // given
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+      // when
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+      // then
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+      });
+    });
   });
 });
