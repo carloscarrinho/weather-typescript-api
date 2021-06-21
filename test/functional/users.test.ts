f5af7d3e0dece2312c84032146832f424de428bd
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-21 16:45:49 -0300

    test: create an user successfully

---
+++ test/functional/users.test.ts
@@ -0,0 +1,23 @@
+import { User } from '@src/models/user';
+
+describe('Users functional tests', () => {
+  beforeEach(async () => {
+    await User.deleteMany();
+  });
+
+  describe('When creating a new user', () => {
+    it('should successfully create an user', async () => {
+      // GIVEN
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      // WHEN
+      const response = await global.testRequest.post('/users').send(newUser);
+      // THEN
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual(expect.objectContaining(newUser));
+    });
+  });
+});
