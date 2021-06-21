0f6927bd1277b996e0adaae6b325e8cda61a29e4
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-21 19:05:12 -0300

    test: return 422 status code when there is a validation error on user creation

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -19,5 +19,21 @@ describe('Users functional tests', () => {
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newUser));
     });
+
+    it('should return 422 status when there is a validation error', async () => {
+      // GIVEN
+      const newUser = {
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      // WHEN
+      const response = await global.testRequest.post('/users').send(newUser);
+      // THEN
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        code: 422,
+        error: 'User validation failed: name: Path `name` is required.',
+      });
+    });
   });
 });
