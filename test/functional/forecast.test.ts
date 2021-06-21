41d1d79b11686559737e4e295f193d09440ec7f8
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-21 07:35:36 -0300

    test: return 500 if something goes wrong during forecast processing

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -33,7 +33,7 @@ describe('Beach forecast functional tests', () => {
         source: 'noaa',
       })
       .reply(200, stormGlassWeather3HoursFixture);
-      
+
     // WHEN
     const { body, status } = await global.testRequest.get('/forecast');
 
@@ -42,5 +42,23 @@ describe('Beach forecast functional tests', () => {
     expect(status).toBe(200);
     // toEqual: Used when you want to check that two objects have the same value.
     expect(body).toEqual(apiForecastResponse1BeachFixture);
+  });
+
+  it('should return 500 if something goes wrong during forecast processing', async () => {
+    // GIVEN
+    nock('https://api.stormglass.io:443', {
+      encodedQueryParams: true,
+      reqheaders: {
+        Authorization: (): boolean => true,
+      },
+    })
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/v2/weather/point')
+      .query({ lat: '-33.792726', lng: '151.289824' })
+      .replyWithError('Something went wrong');
+    // WHEN
+    const { status } = await global.testRequest.get('/forecast');
+    // THEN
+    expect(status).toBe(500);
   });
 });
