4290bd8ca152e57a2c27f845170a445d3fa81194
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-21 08:00:57 -0300

    refactor: remove duplicated types 'Beach' and 'BeachPosition'

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -1,12 +1,7 @@
 import { StormGlass } from '@src/clients/stormGlass';
+import { Beach, BeachPosition } from '@src/models/beach';
 import StormGlassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_3_hours.json';
-import {
-  Forecast,
-  Beach,
-  BeachPosition,
-  TimeForecast,
-  ForecastProcessingInternalError,
-} from '../forecast';
+import { Forecast, ForecastProcessingInternalError } from '../forecast';
 
 jest.mock('@src/clients/stormGlass');
 
@@ -22,7 +17,6 @@ describe('Forecast Service', () => {
         lng: 151.289824,
         name: 'Manly',
         position: BeachPosition.E,
-        user: 'some-id',
       },
     ];
 
@@ -109,7 +103,6 @@ describe('Forecast Service', () => {
         lng: 151.289824,
         name: 'Manly',
         position: BeachPosition.E,
-        user: 'some-id',
       },
     ];
 
