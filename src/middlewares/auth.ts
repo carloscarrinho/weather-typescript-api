6b6e75b8aa7a06d7a356a9b6ce026ccf34fde889
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-22 07:51:57 -0300

    refactor: handle errors on user authentication

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,11 +3,18 @@ import { Request, Response, NextFunction } from 'express';
 
 export function authMiddleware(
   req: Partial<Request>,
-  _: Partial<Response>,
+  res: Partial<Response>,
   next: NextFunction
 ): void {
   const token = req.headers?.['x-access-token'];
-  const decoded = AuthService.decodeToken(token as string);
-  req.decoded = decoded;
-  next();
+  try {
+    const decoded = AuthService.decodeToken(token as string);
+    req.decoded = decoded;
+    next();
+  } catch (err: any) {
+    res.status?.(401).send({
+      code: 401,
+      error: err.message,
+    });
+  }
 }
