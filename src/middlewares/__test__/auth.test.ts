e6efcdf3cd670ed6681fcca6ef560e7081dfd668
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-22 07:52:54 -0300

    test: return unauthorized when there's any problem with the user token

--- src/middlewares/__test__/auth.test.ts
+++ src/middlewares/__test__/auth.test.ts
@@ -17,4 +17,50 @@ describe('Auth Middleware', () => {
     // THEN
     expect(nextFake).toHaveBeenCalled();
   });
+
+  it('should return UNAUTHORIZED if there is a problem on token verification.', async () => {
+    // GIVEN
+    const reqFake = {
+      headers: {
+        'x-access-token': 'invalid-token',
+      },
+    };
+    const sendMock = jest.fn();
+    const resFake = {
+      status: jest.fn(() => ({
+        send: sendMock
+      })),
+    };
+    const nextFake = jest.fn();
+    // WHEN
+    authMiddleware(reqFake, resFake as object, nextFake);
+    // THEN
+    expect(resFake.status).toHaveBeenCalledWith(401);
+    expect(sendMock).toHaveBeenCalledWith({
+      code: 401,
+      error: 'jwt malformed',
+    });
+  });
+
+  it('should return UNAUTHORIZED if there is no token.', async () => {
+    // GIVEN
+    const reqFake = {
+      headers: {},
+    };
+    const sendMock = jest.fn();
+    const resFake = {
+      status: jest.fn(() => ({
+        send: sendMock
+      })),
+    };
+    const nextFake = jest.fn();
+    // WHEN
+    authMiddleware(reqFake, resFake as object, nextFake);
+    // THEN
+    expect(resFake.status).toHaveBeenCalledWith(401);
+    expect(sendMock).toHaveBeenCalledWith({
+      code: 401,
+      error: 'jwt must be provided',
+    });
+  });
 });
