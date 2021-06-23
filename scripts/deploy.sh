84eb3b47aa733464b9747d24f1adaf5e79a4a406
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-23 08:12:01 -0300

    fix-config: master to main and clean up deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-## add a directory called '.ssh'
+
+# Init .ssh dir and expand $SSH_KEY
 mkdir -p ~/.ssh
-## copy ssh key from environmnet variable to a file 'id_rsa'
 echo -e "${SSH_KEY//_/\\n}" > ~/.ssh/id_rsa
-## give permitions to this file
-chmod 600 ~/.shh/id_rsa
-## add umbler host as a known host
+chmod 600 ~/.ssh/id_rsa
+
+# Add umbler's git to the list of known_hosts to prevent terminal for asking to confirm the host
 ssh-keyscan -p 9922 -t rsa geonosis.deploy.umbler.com 2>&1 >> ~/.ssh/known_hosts
