744e75f1dd6cdde9160b973ba804d329fba3796e
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-23 07:29:39 -0300

    fix-config: setup correctly echo command on deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 ## add a directory called '.ssh'
 mkdir -p ~/.ssh
 ## copy ssh key from environmnet variable to a file 'id_rsa'
-echo -e "${SSH_KEY}//_/\\n" > ~/.ssh/id_rsa
+echo -e "${SSH_KEY//_/\\n}" > ~/.ssh/id_rsa
 ## give permitions to this file
 chmod 600 ~/.shh/id_rsa
 ## add umbler host as a known host
