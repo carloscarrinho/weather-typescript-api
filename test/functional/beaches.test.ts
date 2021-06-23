060640d0e66558ae450845c8219ff3a82d644dc0
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-23 05:17:35 -0300

    test: requests for 'beaches' routes should provide a token

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,34 +1,54 @@
 import { Beach } from '@src/models/beach';
+import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 
 describe('Beaches functional tests', () => {
-  beforeAll(async () => await Beach.deleteMany({}));
+  const defaultUser = {
+    name: 'John Doe',
+    email: 'john@mail.com',
+    password: '1234',
+  };
+  let token: string;
+  beforeEach(async () => {
+    await Beach.deleteMany({});
+    await User.deleteMany({});
+    const user = await new User(defaultUser).save();
+    token = AuthService.generateToken(user.toJSON());
+  });
+
   describe('When creating a beach', () => {
     it('should create a beach with success', async () => {
-      // given
+      // GIVEN
       const newBeach = {
         lat: -33.792726,
         lng: 151.289824,
         name: 'Manly',
         position: 'E',
       };
-      // when
-      const response = await global.testRequest.post('/beaches').send(newBeach);
-      // then
+      // WHEN
+      const response = await global.testRequest
+        .post('/beaches')
+        .set({ 'x-access-token': token })
+        .send(newBeach);
+      // THEN
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach));
     });
 
     it('should return 422 when there is a validation error', async () => {
-      // given
+      // GIVEN
       const newBeach = {
         lat: 'invalid_string',
         lng: 151.289824,
         name: 'Manly',
         position: 'E',
       };
-      // when
-      const response = await global.testRequest.post('/beaches').send(newBeach);
-      // then
+      // WHEN
+      const response = await global.testRequest
+        .post('/beaches')
+        .set({ 'x-access-token': token })
+        .send(newBeach);
+      // THEN
       expect(response.status).toBe(422);
       expect(response.body).toEqual({
         error:
@@ -38,11 +58,9 @@ describe('Beaches functional tests', () => {
 
     it.skip('should return 500 status code when there is any other error than type validation error', async () => {
       // TODO: think a way to test this
-      // given
-      
-      // when
-    
-      // then
+      // GIVEN
+      // WHEN
+      // THEN
     });
   });
 });
