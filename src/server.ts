781c36c5880f079d61b2ba6d61e5a7feabc9f3ad
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-25 06:44:34 -0300

    feat: apply express pino logger and cors

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
 import * as database from '@src/database';
+import expressPino from 'express-pino-logger';
+import cors from 'cors';
 import { BeachesController } from './controllers/beaches';
 import { UsersController } from './controllers/users';
 import logger from './logger';
@@ -40,6 +42,16 @@ export class SetupServer extends Server {
   // private setups
   private setupExpress(): void {
     this.app.use(bodyParser.json());
+    this.app.use(
+      expressPino({
+        logger,
+      })
+    );
+    this.app.use(
+      cors({
+        origin: '*',
+      })
+    );
   }
 
   private setupControllers(): void {
