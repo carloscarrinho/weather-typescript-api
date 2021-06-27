1c33150bcf716e67401e7429aef49bdbc6a1b0e1
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-27 08:06:16 -0300

    feat: create rating service get rating for swell period

--- src/services/rating.ts
+++ src/services/rating.ts
@@ -7,12 +7,26 @@ export class Rating {
     wavePosition: BeachPosition,
     windPosition: BeachPosition
   ): number {
-    if(wavePosition === windPosition) return 1;
-    if(this.isOffShoreWind(wavePosition, windPosition)) return 5;
+    if (wavePosition === windPosition) return 1;
+    if (this.isOffShoreWind(wavePosition, windPosition)) return 5;
     return 3;
   }
 
-  private isOffShoreWind(wavePosition: BeachPosition, windPosition: BeachPosition): boolean {
-    return ("NESW".indexOf(wavePosition) + "NESW".indexOf(windPosition)) % 2 == 0;;
+  public getRatingForSwellPeriod(period: number): number {
+    // suggestion: grades for each period
+    // let rating = [1,1,1,1,1,1,1,2,2,2,4,4,4,4,5];
+    if (period < 7) return 1;
+    if (period >= 7 && period < 10) return 2;
+    if (period >= 10 && period < 14) return 4;
+    return 5;
+  }
+
+  private isOffShoreWind(
+    wavePosition: BeachPosition,
+    windPosition: BeachPosition
+  ): boolean {
+    return (
+      ('NESW'.indexOf(wavePosition) + 'NESW'.indexOf(windPosition)) % 2 == 0
+    );
   }
 }
