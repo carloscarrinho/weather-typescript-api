893db570af18ced9d1c3b90345bc482d795f0e13
Carlos Eduardo F. Carrinho <carrinhoedu@gmail.com>
2021-06-27 08:06:56 -0300

    test: calculate beach rating based on a given swell period

--- src/services/__test__/rating.test.ts
+++ src/services/__test__/rating.test.ts
@@ -49,4 +49,42 @@ describe('Rating Service', () => {
       expect(rating).toBe(5);
     });
   });
+  
+  describe('Get rating based on swell period', () => {
+    it('should get a rating of 1 for a period less than 7 seconds', async () => {
+      // WHEN
+      const ratingFor1 = defaultRating.getRatingForSwellPeriod(1);
+      const ratingFor6 = defaultRating.getRatingForSwellPeriod(6);
+      // THEN
+      expect(ratingFor1).toBe(1);
+      expect(ratingFor6).toBe(1);
+    });
+    
+    it('should get a rating of 2 for a period between 7 and 9 seconds', async () => {
+      // WHEN
+      const ratingFor7 = defaultRating.getRatingForSwellPeriod(7);
+      const ratingFor9 = defaultRating.getRatingForSwellPeriod(9);
+      // THEN
+      expect(ratingFor7).toBe(2);
+      expect(ratingFor9).toBe(2);
+    });
+
+    it('should get a rating of 4 for a period between 10 and 13 seconds', async () => {
+      // WHEN
+      const ratingFor10 = defaultRating.getRatingForSwellPeriod(10);
+      const ratingFor13 = defaultRating.getRatingForSwellPeriod(13);
+      // THEN
+      expect(ratingFor10).toBe(4);
+      expect(ratingFor13).toBe(4);
+    });
+
+    it('should get a rating of 5 for a period higher than 13 seconds', async () => {
+      // WHEN
+      const ratingFor14 = defaultRating.getRatingForSwellPeriod(14);
+      const ratingFor16 = defaultRating.getRatingForSwellPeriod(16);
+      // THEN
+      expect(ratingFor14).toBe(5);
+      expect(ratingFor16).toBe(5);
+    });
+  });
 });
